72ab92b649de3a3b4977a4a7d3de46b9182eeb6d
angganyobaIT <anggaradennise7@gmail.com>
2026-06-10 17:02:53 +0700

    SMTP DEBUG 8

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,16 +1,18 @@
-const nodemailer =
-    require("nodemailer");
+const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
-    port: 587,
+    port: 2525,
     secure: false,
 
     auth: {
         user: process.env.BREVO_USER,
         pass: process.env.BREVO_SMTP_KEY,
     },
+
+    connectionTimeout: 30000,
+    greetingTimeout: 30000,
+    socketTimeout: 30000,
 });
 
-module.exports =
-    transporter;
+module.exports = transporter;
